cabe830bc160b045ee9d2147bed8f697d945f523
Benjamin David Shields <benjaminshields@email.arizona.edu>
2016-12-02 00:48:30 -0700

    preparing to refactor recursive invocation

--- MJCPA5/src/Util.hs
+++ MJCPA5/src/Util.hs
@@ -57,9 +57,11 @@ data AST
     | ByteCast      AST
     | ParenExp      AST
 
-    | Instance      AST String      -- new XYZ() creates an instance AST which has a class name and 
-                                    -- a child that should be invoke
-    | Invoke        [AST] String    -- invoke is the child of an instance, it has a method name and a list of expression parameters as children
+    -- | Instance      AST String      -- new XYZ() creates an instance AST which has a class name and 
+    --                                 -- a child that should be invoke
+    -- | Invoke        [AST] String    -- invoke is the child of an instance, it has a method name and a list of expression parameters as children
+    | Instance      AST String      -- new XYZ() creates an instance AST which has a class name
+    | Invoke        AST [AST] String    -- invoke is the parent of an instance, it has an instance, list of expression parameters, and method name as children
 
     -- PA5 Stuff
     | IntArrayInstance AST          -- Child node is the capacity which may be an expression which needs to be evaluated
--- MJCPA5/test/Access0.java
+++ MJCPA5/test/Access0.java
@@ -1,25 +1,61 @@
 import meggy.Meggy;
 
-class Assign0{
-	public static void main(String[] args){
-		new C().test();
-	}
+class Access0{
+    public static void main(String[] args){
+        new C().test();
+    }
 }
 
 class C{
 
-	int[] deep;
+    int[] deep;
+    D mine;
+    D mine2;
 
-	public int[] test2(){
-		deep = new int[5];
-		deep[0] = 3;
-		return deep;
-	}
+    public int[] test2(){
+        deep = new int[5];
+        deep[0] = 3;
+        return deep;
+    }
 
-	public void test(){
-		int heyo;
-		heyo = this.test2().test3().length;
-		//heyo = this.test2()[0];
-		//this.test2()[0] = 3;
-	}
+    public D test3(){
+        mine2 = new D();
+        return mine2;
+    }
+
+    public void test(){
+        int a;
+        int b;
+        int c;
+        int d;
+
+        //this.test2();
+        deep[1] = 4; // MJ.jar doesn't like this
+        mine = new D();
+        a = this.test3().init().length;
+        //b = mine.vari; // MJ.jar doesn't like this
+        // c = this.mine.hard().vari; // MJ.jar doesn't like this
+        d = this.test2()[0];
+    }
+}
+
+class D{
+    int vari;
+    int[] dArr;
+
+    public void set(){
+        vari = 3;
+    }
+
+    public int[] init(){
+        dArr = new int[3];
+        return dArr;
+    }
+
+    public D hard(){
+        D soHard;
+        soHard = new D();
+        soHard.set();
+        return soHard;
+    }
 }
