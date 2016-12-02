ea8aa9af710b4fcb659148441144982fe281fc26
Theodore Sackoss <theodorejsackos@email.arizona.edu>
2016-12-01 22:08:39 -0700

    Okay, so AST should now correctly generate without failing no matter what identifiers appear in the source code (using function id instead of show). Array stuff works in parser now.

---
+++ MJCPA5/test/Array3.java
@@ -0,0 +1,15 @@
+import meggy.Meggy;
+
+class Array3{
+	public static void main(String[] args){
+		new C().test();
+	}
+}
+
+class C{
+	public void test(){
+		int x;
+		if(someArray.length == otherArray[x])
+			Meggy.delay(100);
+	}
+} 
---
+++ MJCPA5/test/Array4.java
@@ -0,0 +1,14 @@
+import meggy.Meggy;
+
+class Array4{
+	public static void main(String[] args){
+		new C().test();
+	}
+}
+
+class C{
+	public void test(){
+		if(someArray[someArray.length] == 0)
+			Meggy.delay(100);
+	}
+} 
