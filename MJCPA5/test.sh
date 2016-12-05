e1db09f79437625cbacce198b2bdf59c69d5c69b
Benjamin David Shields <benjaminshields@email.arizona.edu>
2016-12-04 20:17:53 -0700

    We are being very productive right now doing bash scripts

--- MJCPA5/test.sh
+++ MJCPA5/test.sh
@@ -1,12 +1,63 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-#Script template for running and testing the project without having to type commands
+function cmp() {
+    PARAM="$1"
+    SOURCE="test/$PARAM"
+
+    output=`stack exec MJCPA5 "$SOURCE"` # can possibly use suppress
+
+    if [ $? == 0 ]; then 
+        mv "$SOURCE.s" ./ours.s
+        ours=0
+    else
+        ours=1
+    fi
+
+    outpus=`java -jar MJ.jar "$SOURCE"`
+    if [ $? == 0 ]; then 
+        mv "$SOURCE.s" ./ref.s
+        theirs=0
+    else
+        theirs=1
+    fi
+
+    if [ $theirs -o $ours ]; then
+        if [ $theirs -ne $ours ]; then
+            echo ""
+            res="different"
+        else
+            res=""
+        fi
+    else
+        java -jar MJSIM.jar -b -f ref.s  > ./ref.batch.out
+        java -jar MJSIM.jar -b -f ours.s > ./ours.batch.out
+        res=`diff ours.batch.out ref.batch.out`
+    fi
+}
 
 clear
 
 echo "Usage: bash test.sh"
 
-# don't use spaces around = ?
-FILES=`ls test/Passing` # ` surrounding a command gets it's result as a string
+stack build MJCPA5 
 
-echo "$FILES"
+if [ $? == 0 ]; then
+    # don't use spaces around '=' ?
+    rm test/Passing/*.dot
+    rm test/Passing/*.png
+    FILES=$(ls test/Passing) # `` surrounding a command gets its result as a string
+
+    res=""
+    for x in $FILES
+    do
+        cmp "Passing/$x"
+        if [$res == ""]; then
+            echo "test $x passed"
+        else
+            echo "--------------"
+            echo "TEST $x FAILED"
+            echo "--------------"
+        fi
+    done
+    rm test/Passing/*.dot
+fi
