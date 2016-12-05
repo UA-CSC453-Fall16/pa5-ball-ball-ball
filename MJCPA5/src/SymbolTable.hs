0d9c233e54f7f46b3470dadb2d5f9614fefbdc95
Benjamin David Shields <benjaminshields@email.arizona.edu>
2016-12-05 02:59:05 -0700

    Ascii art!

--- MJCPA5/src/SymbolTable.hs
+++ MJCPA5/src/SymbolTable.hs
@@ -1,10 +1,12 @@
 {-
+                                    < View in "Raw" on GitHub >
 ███████╗██╗   ██╗███╗   ███╗██████╗  ██████╗ ██╗  ████████╗ █████╗ ██████╗ ██╗     ███████╗   ██╗  ██╗███████╗
 ██╔════╝╚██╗ ██╔╝████╗ ████║██╔══██╗██╔═══██╗██║  ╚══██╔══╝██╔══██╗██╔══██╗██║     ██╔════╝   ██║  ██║██╔════╝
 ███████╗ ╚████╔╝ ██╔████╔██║██████╔╝██║   ██║██║     ██║   ███████║██████╔╝██║     █████╗     ███████║███████╗
 ╚════██║  ╚██╔╝  ██║╚██╔╝██║██╔══██╗██║   ██║██║     ██║   ██╔══██║██╔══██╗██║     ██╔══╝     ██╔══██║╚════██║
 ███████║   ██║   ██║ ╚═╝ ██║██████╔╝╚██████╔╝███████╗██║   ██║  ██║██████╔╝███████╗███████╗██╗██║  ██║███████║
 ╚══════╝   ╚═╝   ╚═╝     ╚═╝╚═════╝  ╚═════╝ ╚══════╝╚═╝   ╚═╝  ╚═╝╚═════╝ ╚══════╝╚══════╝╚═╝╚═╝  ╚═╝╚══════╝
+                                    < View in "Raw" on GitHub >
 -}
 --
 -- Not a step in compiling.
@@ -18,6 +20,21 @@ import Data.Map as M
 import Util
 
 import Debug.Trace
+
+-- ========================= SymbolTable data structure
+
+-- Scope is outermost program scope and the stack of strings keeps track
+-- of our current scope nesting.
+data SymbolTable = SymTab Scope [String]
+    deriving (Show,Eq)
+
+type Scope = M.Map String STE
+
+data STE
+    =   ClassSTE    String Scope Int -- String is name, int is the max offset for member variables
+    |   MethodSTE   String (Scope, TypeSig) Int -- String is name, int is the max offset for params and local variables
+    |   VarSTE      Type String String Int -- Type, Name, Base, Offset
+    deriving (Show,Eq)
 
 -- ========================= Functions for SymbolTable
 emptyScope :: Scope
@@ -208,6 +225,32 @@ lookupVariableType st@(SymTab progScope [classname]) vname =
             (Just (VarSTE vartype name base offset)) -> vartype
             (Just x) -> error ("Variable without a type, "++(symTabToString st 0))
 
+{-
+      _____                   _______                   _____                   _______         
+     /\    \                 /::\    \                 /\    \                 /::\    \        
+    /::\    \               /::::\    \               /::\    \               /::::\    \       
+    \:::\    \             /::::::\    \             /::::\    \             /::::::\    \      
+     \:::\    \           /::::::::\    \           /::::::\    \           /::::::::\    \     
+      \:::\    \         /:::/~~\:::\    \         /:::/\:::\    \         /:::/~~\:::\    \    
+       \:::\    \       /:::/    \:::\    \       /:::/  \:::\    \       /:::/    \:::\    \   
+       /::::\    \     /:::/    / \:::\    \     /:::/    \:::\    \     /:::/    / \:::\    \  
+      /::::::\    \   /:::/____/   \:::\____\   /:::/    / \:::\    \   /:::/____/   \:::\____\ 
+     /:::/\:::\    \ |:::|    |     |:::|    | /:::/    /   \:::\ ___\ |:::|    |     |:::|    |
+    /:::/  \:::\____\|:::|____|     |:::|    |/:::/____/     \:::|    ||:::|____|     |:::|    |
+   /:::/    \::/    / \:::\    \   /:::/    / \:::\    \     /:::|____| \:::\    \   /:::/    / 
+  /:::/    / \/____/   \:::\    \ /:::/    /   \:::\    \   /:::/    /   \:::\    \ /:::/    /  
+ /:::/    /             \:::\    /:::/    /     \:::\    \ /:::/    /     \:::\    /:::/    /   
+/:::/    /               \:::\__/:::/    /       \:::\    /:::/    /       \:::\__/:::/    /    
+\::/    /                 \::::::::/    /         \:::\  /:::/    /         \::::::::/    /     
+ \/____/                   \::::::/    /           \:::\/:::/    /           \::::::/    /      
+                            \::::/    /             \::::::/    /             \::::/    /       
+                             \::/____/               \::::/    /               \::/____/        
+                              ~~                      \::/____/                 ~~              
+                                                       ~~                                       
+                                                                                                
+GET RID OF THAT EVIL PATTERN RIGHT BELOW
+-}
+
 lookupVariableType st vname = trace("vname: "++vname++"\n"++(symTabToString st 0)) VoidType
 
 -- Given some current scope, and a variable name, lookup the lower offset of the variable
@@ -346,3 +389,12 @@ getSTEs [] n = ""
 indent :: Int -> String
 indent 0 = ""
 indent n = "\t" ++ indent (n-1)
+
+-- ========================= Testing
+-- Testing
+checkExpect:: (Eq a) => (Show a) => a -> a -> String
+checkExpect check expect =
+    if check /= expect
+    then "checkExpect FAILED, got: "
+         ++ (show check) ++ ", expected: " ++ (show expect)
+    else "checkExpect PASSED"
