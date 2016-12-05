ee102b083f3900d5e5fd66be65611ef87a7959a4
Theodore Sackoss <theodorejsackos@email.arizona.edu>
2016-12-04 20:51:15 -0700

    fixed if statements not being able to be followed by a return statement

---
+++ MJCPA5/test/ArrayAccess.java
@@ -0,0 +1,19 @@
+import meggy.Meggy;
+
+class Example{
+	public static void main(String[] args){
+		new Test().testArrays().possiblyValid();
+	}
+}
+class Test{
+	int [] intArr;
+	public Test testArrays(){
+		if(Meggy.checkButton(Meggy.Button.B))
+				intArr = new int[50];
+		return this;
+	}
+
+	public int possiblyValid(){
+		return intArr[15]; // Impossible to tell if this is valid at compile time.
+	}
+}
